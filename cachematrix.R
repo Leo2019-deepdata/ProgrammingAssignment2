23f0389b3158fb51f1252baad79e1933965c0f00
Leo2019-deepdata <57777516+Leo2019-deepdata@users.noreply.github.com>
2020-08-18 13:16:31 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  z <- NULL        ## initializing inverse as NULL
+  set <- function(y){
+    x <<- y
+    z <<- NULL
+  }
+  get <- function()x    ## function to get matrix X
+  setInverse <- function(inverse) z <<- inverse
+  getInverse <- function() z 
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  z <- x$getInverse()
+  if(!is.null(z)){
+    message("getting cached data")
+    return(z)
+  }
+  tat <- x$get()
+  z <- solve(tat,...)
+  x$setInverse(z)
+  z
 }
